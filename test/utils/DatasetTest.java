7fa5febddf0f56fb68e3a5d6a7a5cc4ec9fb4565
John Vines <jvines@gmail.com>
2011-11-13 10:32:45 -0500

    missed one

--- test/utils/DatasetTest.java
+++ test/utils/DatasetTest.java
@@ -5,25 +5,27 @@ import static org.junit.Assert.*;
 import java.io.File;
 import java.io.IOException;
 
+import javax.naming.OperationNotSupportedException;
+
 import org.junit.Test;
 
 public class DatasetTest {
 
 	@Test
-	public void testList() throws IOException {
+	public void testList() throws IOException, OperationNotSupportedException {
 		Dataset ds = new ListDataset();
 		test(ds);
 	}
 
 	@Test
-	public void testFile() throws IOException
+	public void testFile() throws IOException, OperationNotSupportedException
 	{
 		File testFile = new File("~/tmp");
 		Dataset ds = new FileDataset(testFile,1);
 		test(ds);
 	}
 
-	public void test(Dataset ds) throws IOException
+	public void test(Dataset ds) throws IOException, OperationNotSupportedException
 	{
 		Data d1, d2, d3, d4;
 		d1 = new Data("1.0,2.0,3.0,fish");
